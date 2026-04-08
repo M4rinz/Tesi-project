1ad16bb43976638fcf7158c9ff20cea90911da4a
Andrea Marino <134485532+M4rinz@users.noreply.github.com>
2026-04-08 09:13:42 +0200

    Tests for the computation of the approximants

---
+++ Codice/test/Pade_exp_PS_mk1.jl
@@ -0,0 +1,104 @@
+using LinearAlgebra, Random, Printf
+using Revise
+
+Revise.includet(joinpath(@__DIR__,"..","src","modules","MyMpExponential.jl"))
+Revise.includet(joinpath(@__DIR__,"..","src","modules","MyHelper.jl"))
+using .MyMpExponential, .MyHelper
+
+Random.seed!(42)
+
+
+## Define parameters and useful stuff
+s = 4;      # For example...
+m_tayl_arr = [floor(Int64, (i+2)^2/4) for i=1:10];   # Optimal degrees for Taylor
+m_pade_arr = [1, 2];
+for i=2:length(m_tayl_arr)-1
+    bi = 2*ceil(Int64, (i-1)/4)*(i-1-2*div(i-2,4))+1
+    push!(m_pade_arr, bi)
+end                                                  # Optimal degrees for Padé 
+                                
+function horner(X, coeffs, s)
+    n = LinearAlgebra.checksquare(X)
+    scaling = 2^s
+
+    Xsc = X / scaling
+    Y = copy(Xsc)
+
+    length(coeffs) == 1 && return coeffs[1] * I(n)
+
+    Y = coeffs[end] * Y + coeffs[end-1] * I(n)
+    for k = length(coeffs)-2:-1:1
+        Y = Y * Xsc + coeffs[k] * I(n)
+    end
+
+    return Y
+end
+
+
+## Check symbolically
+# using Symbolics
+# @variables a b
+
+# M = Diagonal([a, b])
+# Mpows = [I(size(M,1)), M^2];
+# m = m_tayl_arr[2]
+
+# M_tayl_true = simplify.(expand.(tayl_exp_horner(M, m, 0)))
+
+# M_pade = expm_diagonal!(M, Mpows, m, 0, cheap_r=false)
+
+print("The Symbolic check is experiencing some issues (and is not particularly revealing anyway)\n")
+
+
+## First numerical check
+print("We check our `expm_diagonal!` against what is obtained with the Horner evaluation scheme "
+ * "(for the same Padé approximant of course), using a small random BigFloat matrix\n")
+
+n = 5;
+A = rand(BigFloat, n, n);
+
+m = big(m_pade_arr[7])
+pade_num_coeffs = [binomial(m,j)*factorial(2m-j)/factorial(2m) for j=0:m];
+pade_den_coeffs = (-1).^(0:m) .* pade_num_coeffs;
+Pₘ  = horner(A, pade_num_coeffs, 0);
+Qₘ  = horner(A, pade_den_coeffs, 0);
+Y_h = Qₘ \ Pₘ;
+
+Apows = [I(n), A^2];
+Y_ps = expm_diagonal!(A, Apows, m, 0);
+@printf("|| Y_ps - Y_h || / || Y_h || = %.4g\n", rel_err(Y_ps, Y_h))
+print("Y_ps ≈ Y_h is $(Y_ps ≈ Y_h)\n")
+print("Y_ps ≈ Y_h (up to machine precision) is $(isapprox(Y_ps, Y_h, rtol=eps(BigFloat)))\n")
+
+
+
+## Second numerical check
+print("We construct a \"tame\" matrix for whose the Padé approx. of the matrix "
+ * "exponential works well, and compare our methods to evaluate rational functions (basically)\n")
+
+A  = rand(n, n);
+A  = (A + A')/2;
+A -= (tr(A)/n) * I(n)   # shift 
+
+Y_true = exp(Float64.(A));
+
+m = big(m_pade_arr[7])
+
+Pₘ = horner(A, pade_num_coeffs, 0);
+Qₘ = horner(A, pade_den_coeffs, 0);
+Y_h = Qₘ \ Pₘ;
+
+@printf("|| Y_h - exp(A) || / || exp(A) || = %.4g\n", rel_err(Y_h, Y_true))
+print("Y_h ≈ exp(A) is $(Y_h ≈ Y_true)\n")
+
+Apows = [I(n), A^2];
+Y_ps = expm_diagonal!(A, Apows, m, 0);
+@printf("|| Y_ps - exp(A) || / || exp(A) || = %.4g\n", rel_err(Y_ps, Y_true))
+print("Y_ps ≈ exp(A) is $(Y_ps ≈ Y_true)\n")
+
+print("Ok, we've obtained good approximations of exp(A). But the crucial point is: how close are Y_h and Y_ps?\n")
+@printf("|| Y_ps - Y_h || / || Y_h || = %.4g\n", rel_err(Y_ps, Y_h))
+print("Y_ps ≈ Y_h (up to (Float64) machine precision) is $(isapprox(Y_ps, Y_h, rtol=eps()))\n")
+
+print("Note that eltype(A) = $(eltype(A)), eltype(Y_h) = $(eltype(Y_h)), eltype(Y_ps) = $(eltype(Y_ps))\n")
+print("\n")
--- Codice/test/Taylor_exp_PS_mk1.jl
+++ Codice/test/Taylor_exp_PS_mk1.jl
@@ -1,7 +1,8 @@
-using LinearAlgebra, Random
+using LinearAlgebra, Random, Printf
+using Revise
 
-include(joinpath(@__DIR__,"..","src","modules","MyMpExponential.jl"))
-include(joinpath(@__DIR__,"..","src","modules","MyHelper.jl"))
+Revise.includet(joinpath(@__DIR__,"..","src","modules","MyMpExponential.jl"))
+Revise.includet(joinpath(@__DIR__,"..","src","modules","MyHelper.jl"))
 using .MyMpExponential, .MyHelper
 
 Random.seed!(42)
@@ -9,16 +10,6 @@ Random.seed!(42)
 ## Define parameters and useful stuff
 s = 4;          # For example...
 m_array = [floor(Int64, (i+2)^2/4) for i=1:6]   # Optimal degrees for Taylor
-
-function tayl_exp_horner(X, m, s)
-    n = LinearAlgebra.checksquare(X)
-    scaling = 2^s
-    Y = X / (scaling*m)
-    for j = 1:m-1
-        Y = (Y + I(n)) * (X / (scaling * (m - j)))
-    end
-    return Y + I(n)
-end
 
 
 ## Check symbolically
@@ -29,13 +20,13 @@ M = Diagonal([a, b])
 m = 5
 
 Mtrue = simplify.(expand.(tayl_exp_horner(M, m, 0)))
-Mps   = simplify.(expand.(polyvalm_tay_exp(M, m, 0)))
+Mps   = simplify.(expand.(expm_taylor(M, [], m, 0)))
 print("T₅(a) - T̃₅(a) = $(Mtrue[1,1] - Mps[1,1])\n")
 print("This is definitively not the zero polynomial. However, by switching from m = $m to m = $(m_array[3])...\n")
 
 m = m_array[3]
 Mtrue = simplify.(expand.(tayl_exp_horner(M, m, 0)))
-Mps   = simplify.(expand.(polyvalm_tay_exp(M, m, 0)))
+Mps   = simplify.(expand.(expm_taylor(M, [], m, 0)))
 print("Tₘ(a) - T̃ₘ(a) = $(Mtrue[1,1] - Mps[1,1])\n")
 print("The result is now correct.\n")
 
@@ -44,17 +35,20 @@ I think it's due to the numerical evaluation of the symbolic expressions. I'm st
 
 
 ## First numerical check
+print("We check our scheme `expm_taylor` against the Horner evaluation scheme "
+ * "(for the same Taylor polynomial of course), using a small random matrix.\n")
+
 m = m_array[6]  # i.e. m = 16
 n = 5;
 A = rand(n,n)
-print("n = $n. m=$m. eltype(A) = $(eltype(A))\n")
+print("n = $n. m = $m. eltype(A) = $(eltype(A))\n")
 
 Ytrue = tayl_exp_horner(A, m, s)
-Y = polyvalm_tay_exp(A, m, s)
+Y     = expm_taylor(A, [], m, s)
 
 print("|| Y - Y_true || / || Y_true || = $(rel_err(Y,Ytrue))\n")
-print("|| exp(A) - Y || / || exp(A) || = $(rel_err(exp(A),Y))\t (m = $m)\n")
-print()
+print("|| exp(A) - Y || / || exp(A) || = $(rel_err(Y,exp(A)))\t (m = $m)\n")
+print("\n")
 
 
 ## Second numerical check
@@ -67,37 +61,36 @@ Y_ref = setprecision(2*precision(eltype(B))) do
 end
 
 Y_horner = tayl_exp_horner(B, m, s)
-print("|| Y_ref - Y_horner || / || Y_ref || = $(rel_err(Y_horner, Y_ref))\n")
-print("And indeed 2^(1-precision(BigFloat)) = eps(BigFloat) = $(eps(BigFloat))\n")
+@printf("|| Y_ref - Y_horner || / || Y_ref || = %.6g\n", rel_err(Y_horner, Y_ref))
+@printf("And indeed 2^(1-precision(BigFloat)) = eps(BigFloat) = %.4g\n", eps(BigFloat))
 print("We're within a factor ≤ 10 from the unit roundoff. E ci mancherebbe altro! Nothing here surprises us.\n")
 
-Y = polyvalm_tay_exp(B, m, s)   # Paterson-Stockmeyer in standard BigFloat precision
-print("|| Y - Y_ref || / || Y_ref || = $(rel_err(Y, Y_ref))\n")
-print("|| Y - Y_ref || / (1 + || Y || + || Y_ref ||) = $(norm(Y - Y_ref) / (1 + norm(Y) + norm(Y_ref))))\n")
-# Domanda 1: qual è più giusto? (Non tra i due, in generale)
+Y = expm_taylor(B, [], m, s)   # Paterson-Stockmeyer in standard BigFloat precision
+@printf("|| Y - Y_ref || / || Y_ref || = %.6g\n", rel_err(Y, Y_ref) )
+@printf("|| Y - Y_ref || / (1 + || Y || + || Y_ref ||) = %.6g\n", sym_err(Y, Y_ref) )
 # Domanda 2: con cosa va confrontato? eps(BigFloat)? 2^(-precision(BigFloat))? 
-#            Con n * ϵ ? O ϵ / n ? (ϵ è uno dei due sopra)
+#            Con n⋅ϵ ? O con ϵ / n ? (ϵ è uno dei due sopra)
 # Domanda 3: Come "mettere tutto insieme" con la funzione `isapprox` (o ≈ insomma)?
 #            Mi pare di capire che || Y - Y_ref || ≈ 0 sia l'approccio sbagliato (cfr. ?> ≈)
 #            Però isapprox usa come tolleranza relativa √eps(T) dove T è il tipo e dipende dagli operandi
 #            Sono incappato in questo : https://discourse.julialang.org/t/approximate-equality/8952
-print()
+print("\n")
 
 
 ## Third numerical check
 n = 50
 B = rand(BigFloat, n, n)
-print("n = $n. m=$m. eltype(B) = $(eltype(B))\n")
+print("n = $n. m = $m. eltype(B) = $(eltype(B))\n")
 
 Y_ref = setprecision(2*precision(eltype(B))) do 
     tayl_exp_horner(B, m, s)
 end
 
 Y_horner = tayl_exp_horner(B, m, s)
-print("|| Y_ref - Y_horner || / || Y_ref || = $(rel_err(Y_horner, Y_ref))\n")
+@printf("|| Y_ref - Y_horner || / || Y_ref || = %.6g\n", rel_err(Y_horner, Y_ref))
 
-Y = polyvalm_tay_exp(B, m, s)
-print("|| Y - Y_ref || / || Y_ref || = $(rel_err(Y, Y_ref))\n")
-print("|| Y - Y_ref || / (1 + || Y || + || Y_ref ||) = $(norm(Y - Y_ref) / (1 + norm(Y) + norm(Y_ref))))\n")
+Y = expm_taylor(B, [], m, s)
+@printf("|| Y - Y_ref || / || Y_ref || = %.6g\n", rel_err(Y, Y_ref))
+@printf("|| Y - Y_ref || / (1 + || Y || + || Y_ref ||) = %.6g\n", sym_err(Y, Y_ref))
 print("Y ≈ Y_ref up to machine precision = $(isapprox(Y, Y_ref, rtol=eps(BigFloat)))\n")
-print()
+print("\n")
