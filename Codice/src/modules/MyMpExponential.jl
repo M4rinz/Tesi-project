f5fab820124b35cde13c52ccbf90a7c78390853b
M4rinz <134485532+M4rinz@users.noreply.github.com>
2026-04-25 11:47:57 +0200

    Come al solito ho fatto casino con git

--- Codice/src/modules/MyMpExponential.jl
+++ Codice/src/modules/MyMpExponential.jl
@@ -105,8 +105,8 @@ function polyvalm_ps!(
     end
 
     # Evaluate B-terms of degree ν-1, and evaluate the main polynomial using Horner
-    Y = convert(Matrix{outputclass}, B) # to retain original precision 
-                                        # (B is computed at higher precision, hopefully)
+    Y = convert(AbstractMatrix{outputclass}, B) # to retain original precision 
+                                                # (B is computed at higher precision, hopefully)
     for kk = r-1:-1:0
         # compute B coeff. in slightly higher precision
         B = setprecision(floor(Int64, 1.2 * precision(BigFloat))) do 
@@ -121,11 +121,7 @@ function polyvalm_ps!(
         # Y è mantenuta in `outputclass`. Però c'è una copia in più.
         # Ho come l'impressione che tutte queste acrobazie con i tipi non servano a niente, 
         # alla fin fine
-
-        #Y = Y * mpowers[ν+1] + convert(AbstractMatrix{outputclass}, B)
-        tmp = convert(AbstractMatrix{outputclass}, B)
-        mul!(tmp, Y, mpowers[ν+1], 1, 1)    # tmp ← Y * mpowers[ν+1] + tmp
-        copyto!(Y, tmp)                     #  Y  ← tmp
+        Y = Y * mpowers[ν+1] + convert(AbstractMatrix{outputclass}, B)
     end
 
     # WIP: questo `outputclass` è ben gestito?
@@ -221,22 +217,35 @@ function expm_diagonal!(
     end
 
     # compute even and odd parts, using even/odd coefficients
-    Uₑ = polyvalm_ps!(S.powers, s, c_num[1:2:end])
-    if m ≥ 1 
-        Uₒ = polyvalm_ps!(S.powers, s, c_num[2:2:end])
-        Uₒ = (S.A / 2^s) * Uₒ
-    else 
-        Uₒ = zeros(eltype(S.A), size(S.A))
+    t_U_e = @elapsed begin 
+        Uₑ = polyvalm_ps!(S.powers, s, c_num[1:2:end])
+    end 
+    VERBOSE ? @printf("\tEvaluating Uₑ took %.6f s\n", t_U_e) : nothing 
+    t_U_o = @elapsed begin 
+        if m ≥ 1 
+            Uₒ = polyvalm_ps!(S.powers, s, c_num[2:2:end])
+            Uₒ *= (S.A / 2^s)
+        else 
+            Uₒ = zeros(eltype(S.A), size(S.A))
+        end
     end
+    VERBOSE ? @printf("\tEvaluating Uₒ took %.6f s\n", t_U_o) : nothing 
 
-    if cheap_r
-        Qₘ = Uₑ - Uₒ
-        return Qₘ \ (2*Uₒ) + I(n)
-    else 
-        Pₘ = polyvalm_ps!(S.powers, s, c_num)
-        Qₘ = polyvalm_ps!(S.powers, s, c_den)
-        return Qₘ \ Pₘ
+    t_R_m = @elapsed begin 
+        if cheap_r
+            Qₘ = Uₑ - Uₒ
+            Rₘ = Qₘ \ (2*Uₒ) + I(n)
+            #return Pₘ
+        else 
+            Pₘ = polyvalm_ps!(S.powers, s, c_num)
+            Qₘ = polyvalm_ps!(S.powers, s, c_den)
+            Rₘ = Qₘ \ Pₘ
+            #return Qₘ \ Pₘ
+        end
     end
+    VERBOSE ? @printf("\tEvaluating Rₘ took %.6f s\n", t_R_m) : nothing
+
+    return Rₘ
 end
 
 
@@ -871,7 +880,7 @@ end
 
 function opt_degs(max_deg::Integer=500, approximant::Symbol=:pade)
     approximant ∈ VALID_APPRX || 
-            throw(ArgumentError("Invalid approximant: $approximant. Valid options are $(VALID_APPROXIMANTS)"))
+            throw(ArgumentError("Invalid approximant: $approximant. Valid options are $(VALID_APPRX)"))
     if approximant == :taylor
         opt_degs_tayl(max_deg)
     else 
@@ -965,7 +974,7 @@ function exp_mp(
 
     #TODO: better verification of the validity of the arguments
     approximant ∈ VALID_APPRX || 
-        throw(ArgumentError("Invalid approximant: $approximant. Valid options are $(VALID_APPROXIMANTS)"))
+        throw(ArgumentError("Invalid approximant: $approximant. Valid options are $(VALID_APPRX)"))
     algorithm ∈ VALID_ALGS || 
         throw(ArgumentError("Invalid algorithm: $algorithm. Valid options are $(VALID_ALGS)"))
         
@@ -1016,7 +1025,8 @@ function exp_mp(
         if cmplx_schur
             F = Schur{Complex}(F)
         end
-        X, Q, _ = F
+        #X, Q, _ = F
+        X = F.T
     end
 
     # shift the input matrix
@@ -1151,10 +1161,22 @@ function exp_mp(
 
         ## Calcolo dell'esponenziale di matrice 
         VERBOSE ? print("Computing approximant start...\n") : nothing
+        VERBOSE ? print("XandP has $(length(XandP.powers)) elements.\n") : nothing
         X /= 2^s    # scaling 
-        X ≈ (A - useshift*μ*I(n)) / 2^s || @warn("X ≈ 2^-s⋅(A-μI) è falso. $(norm(X - (A-μ*I(n))/2^s))")
-        X ≈ (A - useshift*μ*I(n)) / 2^(2s) && @warn("X ≈ 2^-2s⋅(A-μI).")
-        Y = eval_pade!(XandP, m, s) # Y = rₘ(2^(-s)X)
+        
+        if algorithm === :transfree
+            X ≈ (A - useshift*μ*I(n)) / 2^s    || @warn("X ≈ 2^-s⋅(A-μI) è falso. $(norm(X - (A-μ*I(n))/2^s))")
+            X ≈ (A - useshift*μ*I(n)) / 2^(2s) && @warn("X ≈ 2^-2s⋅(A-μI).")
+        else
+            X ≈ (F.T - useshift*μ*I(n)) / 2^s    || @warn("X ≈ 2^-s⋅(T-μI) è falso. $(norm(X - (A-μ*I(n))/2^s))")
+            X ≈ (F.T - useshift*μ*I(n)) / 2^(2s) && @warn("X ≈ 2^-2s⋅(T-μI).")
+        end
+
+        t_eval = @elapsed begin
+            Y = eval_pade!(XandP, m, s) # Y = rₘ(2^(-s)X)
+        end
+        VERBOSE ? @printf("Approximant evaluation time: %.6f s\n", t_eval) : nothing
+
         if recompute_diag_blocks
             recompute_diagonals!(X, Y)  # overwrites Y
         end
@@ -1168,7 +1190,7 @@ function exp_mp(
         ## Squaring 
         VERBOSE ? print("Squaring start...\n") : nothing
         for _ = 1:s
-            Y = Y^2
+            Y *= Y          # Y ← Y²
             if recompute_diag_blocks
                 X *= 2      # X = 2^(-s+t)A if !positive_shift, else 2^(-s+t)(A-μI) 
                 recompute_diagonals!(X, Y)
@@ -1179,7 +1201,10 @@ function exp_mp(
     end # if isdiag(X)
 
     if compute_schur 
-        Y = Q * Y * Q'
+        Y = F.Z * Y * F.Z'
+        #tmp = similar(Y);
+        #mul!(tmp, F.Z, Y)   # Y ← Q * Y
+        #mul!(Y, tmp, F.Z')  # Y ← Y * Q'
     end
     if isreal(A)
         Y = real(Y)
