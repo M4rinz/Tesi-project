1fe5753dba2864d619a028c267785023214ed478
Andrea Marino <134485532+M4rinz@users.noreply.github.com>
2026-05-25 16:40:33 +0200

    `produce_plot` ora supporta altri hues. Chissà che ho fatto col Notebook

--- Dati_benchmarks_et_al/utilities.py
+++ Dati_benchmarks_et_al/utilities.py
@@ -5,19 +5,24 @@ import seaborn as sns
 def produce_plot(
         df:pd.DataFrame,
         ax,
+        hue="exp",
         window=None
     ) -> None:
     # sort rows by value of u * k
     df_sorted = df.loc[(df["epsilon"] * df["cond_expA_F"]).sort_values(ascending=False).index]
 
     # the "hue" column is used to distinguish the configurations
-    df_sorted["hue"]  = "exp" 
-    df_sorted["hue"] += df_sorted["algorithm"].map(
-            lambda a: "_sp" if a in ["realschur", "complexschur"] else ""
-        )
-    df_sorted["hue"] += df_sorted["approximant"].map(
-            lambda a: "_"+"".join([x[0] for x in a.split("_")])
-        )
+    if hue=="Ytrue_method":
+        df_sorted["hue"] = df_sorted["Ytrue_method"]
+    else:
+        df_sorted["hue"]  = "exp" 
+        df_sorted["hue"] += df_sorted["algorithm"].map(
+                lambda a: "_sp" if a in ["realschur", "complexschur"] else ""
+            )
+        df_sorted["hue"] += df_sorted["approximant"].map(
+                lambda a: "_"+"".join([x[0] for x in a.split("_")])
+            )
+    
     df_sorted["err_ubound"] = df_sorted["epsilon"]*df_sorted["cond_expA_F"]
 
     if window:
@@ -30,6 +35,8 @@ def produce_plot(
         df_sorted["err_ubound"] = df_sorted["err_ubound"].map(
             lambda x: x if low<=x<=up else (up if x>up else low)
         )
+        ax.axhline(y=low, color="grey", alpha=0.5, linestyle="--")
+        ax.axhline(y=up, color="grey", alpha=0.5, linestyle="--")
 
     sns.scatterplot(x=range(len(df_sorted)), y=df_sorted["rel_err_F"],
                     ax=ax, hue=df_sorted["hue"], style=df_sorted["hue"])
